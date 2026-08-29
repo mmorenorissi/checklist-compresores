6a7b8ea021b71ee5ba6d1bfd4d6bf35ba1a30eb2
mmorenorissi <msh8nvvn89@privaterelay.appleid.com>
2026-08-29 18:09:36 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'mantenimiento-compresores-ant-v29';
+const CACHE = 'mantenimiento-compresores-ant-v30';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
